8205373d1c0f3ffbab3c27b79e9580c637460b8d
Ali Bagheri <23119@stmarys.ac.uk>
2024-03-22 14:43:41 +0000

    select command

---
+++ Methods/HelloWord.cs
@@ -0,0 +1,22 @@
+public class HelloWorld
+{
+    public static void Main()
+    {
+        SayHello();
+        
+    }
+
+    public static void SayHello()
+    {
+        Console.WriteLine("Hello, World!");
+    }
+    public static int Add(int1,int2)
+    {
+        int result = int1 + int2
+        return result
+    }
+    public static void WriteSomethingSpecific(string myText)
+    {
+        Console.WriteLine(myText)
+    }
+}
